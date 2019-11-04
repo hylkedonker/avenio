d9c625561f85dd65b4f4fb679af9adb44136e72c
Hylke C. Donker <h.c.donker@umcg.nl>
2019-11-04 14:50:17 +0100

    Added model for gene embedding, including unit test.

--- models.py
+++ models.py
@@ -1,10 +1,13 @@
 from typing import Optional
 
 from catboost import CatBoostClassifier
+import gensim
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import OrdinalEncoder
+
+from const import target_genes
 
 
 class UniqueFeatureFilter(BaseEstimator, TransformerMixin):
@@ -49,7 +52,7 @@ class CustomCatBoostClassifier(CatBoostClassifier):
             y=y,
             cat_features=self.cat_features,
             eval_set=self.eval_set,
-            **fit_params
+            **fit_params,
         )
 
 
@@ -97,3 +100,72 @@ class ClassifierAsTransformer(BaseEstimator, TransformerMixin):
             y_output = self.encoder.transform(y_output)
 
         return y_output
+
+
+class Gene2Vec(BaseEstimator, TransformerMixin):
+    """
+    Transform gene columns to gene embeddings.
+
+    X'(i,k) = sum_j X_(ij) * gene(j, k)
+    with gene(j, k) the embedding vector.
+    """
+
+    def __init__(
+        self,
+        embedding_model: gensim.models.word2vec.Word2Vec = None,
+        ignore_unknown_columns: bool = False,
+    ):
+        # Load model if none was passed.
+        if embedding_model is None:
+            self.model = gensim.models.KeyedVectors.load_word2vec_format(
+                "gene2vec_dim_200_iter_9_w2v.txt", binary=False
+            )
+        else:
+            self.model = embedding_model
+
+        self.ignore_ = ignore_unknown_columns
+
+    def fit(self, X: pd.DataFrame, y=None):
+        """
+        Parse columns to process.
+        """
+        # We need the column names for the embeddings.
+        if not isinstance(X, pd.DataFrame):
+            raise TypeError("X must be pandas data frame.")
+
+        if not self.ignore_ and not set(X.columns).issubset(set(target_genes)):
+            raise KeyError(f"Unknown columns in {X.columns}")
+
+        # What columns to use.
+        self.columns_to_transform_ = tuple(
+            column for column in X.columns if column in self.model
+        )
+
+        return self
+
+    def transform(self, X, y=None):
+        """
+        Generate new matrix by summing embedding contributions.
+        """
+        # Allocate memory for storing the embeddings.
+        X_T = np.zeros([X.shape[0], self.model.vector_size])
+
+        for column in self.columns_to_transform_:
+            # Embedding for given gene.
+            v = self.model[column]
+            # Multiply embedding by allele frequency (for each patient). Sum
+            # contributions of all genes.
+            X_T += X[column].values.reshape(-1, 1) * v.reshape(1, -1)
+
+        columns_to_keep = [
+            column
+            for column in X.columns
+            if column not in self.columns_to_transform_
+        ]
+
+        if columns_to_keep:
+            return pd.concat(
+                [X[columns_to_keep], pd.DataFrame(X_T, index=X.index)], axis=1
+            )
+
+        return X_T
--- test/test_models.py
+++ test/test_models.py
@@ -1,10 +1,11 @@
 import unittest
 
+import gensim
 import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
-from models import ClassifierAsTransformer, UniqueFeatureFilter
+from models import ClassifierAsTransformer, UniqueFeatureFilter, Gene2Vec
 
 
 class TestUniqueFeatureFilter(unittest.TestCase):
@@ -44,4 +45,47 @@ class TestClassifierAsTransformer(unittest.TestCase):
         ).fit(X, y)
         np.testing.assert_array_equal(
             tree.predict(X), tree_transformer.transform(X)
+        )
+
+
+class TestGene2Vec(unittest.TestCase):
+    def setUp(self):
+        # Load embedding.
+        self.model = gensim.models.KeyedVectors.load_word2vec_format(
+            "gene2vec_dim_200_iter_9_w2v.txt", binary=False
+        )
+
+    def test_embedding(self):
+        """
+        Test that genes are correctly embedded.
+        """
+        #
+        X = pd.DataFrame(
+            {
+                "TP53": [1.0, 0.0, 2.0],
+                "KRAS": [0.0, 2.0, 1.0],
+                "age": [30, 40, 40],
+            },
+            index=[1, 2, 3],
+        )
+
+        with self.assertRaises(KeyError):
+            Gene2Vec(
+                embedding_model=self.model, ignore_unknown_columns=False
+            ).fit_transform(X)
+
+        X_embed = Gene2Vec(
+            embedding_model=self.model, ignore_unknown_columns=True
+        ).fit_transform(X)
+
+        # Check that the age column is unaffected.
+        pd.testing.assert_series_equal(X_embed["age"], X["age"])
+
+        # Check that the columns are correctly embedded.
+        np.testing.assert_array_equal(X_embed.iloc[0, 1:], self.model["TP53"])
+        # Second patient is a multiple of the vector.
+        np.testing.assert_array_equal(X_embed.iloc[1, 1:], 2 * self.model["KRAS"])
+        # Third patient is linear combination of the two.
+        np.testing.assert_array_almost_equal(
+            X_embed.iloc[2, 1:], 2 * self.model["TP53"] + self.model["KRAS"]
         )
